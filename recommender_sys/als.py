a5799760b2d79baf6245fdd6283a0816d0b7f9bb
ravi thakur <ravithakur@ravis-MacBook-Air.local>
2024-06-09 21:21:50 +0530

    Numpy version of ALS for explicit ratings implemented. basic utilities provided

--- recommender_sys/als.py
+++ recommender_sys/als.py
@@ -11,15 +11,82 @@ to handle them in Numpy
 """
 import numpy as np
 from scipy.sparse import csr_matrix
+from tqdm import tqdm
 
 
-class ALSNum:
-    def __init__(self, ratings: csr_matrix, factors: int, regularization: float):
-        """Alternating Least Squares - NumPy Variant
+def loss_als(ratings, rows, columns, users, items, regular):
+    """
+    Computes the loss of the current ratings approximation R ~ UV^T
+
+    Parameters
+    ----------
+    ratings: scipy.sparse.csr_matrix
+        the sparse user-item explicit ratings matrix
+    rows: np.array
+        array containing indices of the rows of the non-zero elements in ratings.
+        This is required for the loss function evaluations.
+    rows: np.array
+        array containing indices of the columns of the non-zero elements in ratings
+    users: np.ndarray
+        the latent user matrix
+    items: np.ndarray
+        the latent item matrix
+    regular: float
+        the regularization coefficient
+    
+    Returns
+    -------
+    loss: float
+        the regularized ALS loss using approximation of the ratings matrix
+    """
+
+    actual_ratings = ratings.data
+
+    # Compute predictions for all non-zero entries
+    predictions = np.sum(users[rows] * items[columns], axis=1)
+
+    # Calculate the squared differences
+    squared_diff = (actual_ratings - predictions)**2
+
+    # frobenius norm of user and item latent matrices
+    fro_user = np.linalg.norm(users, ord='fro')**2
+    fro_item = np.linalg.norm(items, ord='fro')**2
+
+    # Compute the loss
+    loss = 0.5*(np.sum(squared_diff) + regular*(fro_user + fro_item))
+    return loss
+
+
+def A_inv(mat, regular):
+    """
+    Computes the inverse of SUM(UU^T) + Lambda I to be used in each inner iteration of ALS
+    uu^T = [u_1, u_2, u_3][u_1 
+                        u_2
+                        u_3]
+    regular: float
+        the regularization coefficient
+    Parameters
+    ----------
+    mat: np.ndarray
+        the user or item latent matrix depending on the sub iteration
+    """
+    uut = np.einsum('ij,ik->jk', mat, mat)
+    return np.linalg.inv(uut + regular*np.eye(uut.shape[0]))
+
+
+class ALSNumExpl:
+    def __init__(self, ratings: csr_matrix, factors: int, regularization: float, seed: int=42):
+        """Alternating Least Squares - NumPy Variant - Explicit ratings only
 
         Implements the alternating least squares algorithm to solve the matrix factorization
         problem or the latent factor model to reduce users and items in a recommendation
-        system to a smaller vectors in the latent space.
+        system to a smaller vectors in the latent space. This isn't a very robust implementation
+        that handles missing values in ratings matrix. Assumes all values of ratings are correct.
+
+        This is a personal implementation to model alternative least squares on small datasets.
+        Focus is more on implementation from scratch of the models rather than providing an all
+        purpose interface to ALS. There will also be a tensorflow based model for this. Comparison
+        shall be done on the overall runtimes of these models.
 
         Parameters
         ----------
@@ -29,11 +96,36 @@ class ALSNum:
             The number of latent factors to convert to
         regularization: float
             The regularization term
+        seed: float
+            the random seed for reproducibility
         """
 
+        self.ratings = ratings
         self.factors = factors
         self.regularization = regularization
-        pass
+        self.seed = seed
+
+        # check if the factors is not more than the size of the user-rating matrix
+        if (factors > ratings.shape[0]) or (factors > ratings.shape[1]):
+            raise ValueError("The number of factors cannot be larger than unique count of users or items")
+        
+        self.user_count = ratings.shape[0]
+        self.item_count = ratings.shape[1]
+
+        # regularization term can't be negative
+        if regularization < 0:
+            raise ValueError("Regularization factor can't be negative")
+
+        # initializting the user and item latent factor matrices
+        # using a uniform random small number. Gaussian centered around zero can also be used.
+        # TODO: add Gaussian initial parameters.
+        np.random.seed(seed)
+        self.ulf = np.random.rand(self.user_count, factors)*0.001
+        self.ilf = np.random.rand(self.item_count, factors)*0.001
+
+        # array to store losses
+        self.loss = []
+
 
     def fit(self, iterations: int=100):
         """
@@ -45,7 +137,37 @@ class ALSNum:
         iterations: int
             the number of iterations for which to run the optimization for
         """
-        pass
+        rows, columns = self.ratings.nonzero()
+
+        # initial loss
+        initial_loss = loss_als(self.ratings,
+                                rows,
+                                columns,
+                                self.ulf,
+                                self.ilf,
+                                self.regularization)
+        self.loss.append(initial_loss)
+        
+        for _ in tqdm(range(iterations)):
+            # first update user matrix
+            a_inv = A_inv(self.ilf, self.regularization)
+            B = self.ratings.dot(self.ilf)
+            self.ulf = B @ a_inv.T
+            
+            # update item matrix
+            a_inv = A_inv(self.ulf, self.regularization)
+            B = self.ratings.transpose().dot(self.ulf)
+            self.ilf = B @ a_inv.T
+
+            iteration_loss = loss_als(self.ratings,
+                                      rows,
+                                      columns,
+                                      self.ulf,
+                                      self.ilf,
+                                      self.regularization)
+            self.loss.append(iteration_loss)
+        return self.loss
+
 
     def recommend_top_k_items(self, k: int, user_index: int):
         """
@@ -61,9 +183,4 @@ class ALSNum:
         """
         pass
 
-    def loss_rmse(self):
-        """
-        Calculates and/or returns the overall loss of the model
-        """
-        pass
 
